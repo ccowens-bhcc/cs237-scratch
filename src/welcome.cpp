c5eb339e0bbf125880b164cc5764f19bdcccbace
ccowens-bhcc <christopher.owens01@bhcc.edu>
2024-01-25 10:34:28 -0500

    first time!

---
+++ src/welcome.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/*
+  Welcome to the Grader Than IDE
+
+  To run this file click the play button in the top right corner. Or right click
+  on any blank space in this file and click "Run Code" in the context menu that
+  appears.
+
+  To debug this file:
+    1) Click the play button with the bug on it in the action bar on the left
+       side of the screen
+    2) In the activity menu that appears on the left side of the screen,
+       click the green play botton at the top
+*/
+
+int main() {
+  
+}
